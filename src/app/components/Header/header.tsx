7cd05ffcb32bb4da7e2e9cb6bf942b32f15d0d09
mohammednazim007 <mohammednazim3629@gmail.com>
2025-02-09 19:57:56 +0600

    search input design is done

--- src/app/components/Header/header.tsx
+++ src/app/components/Header/header.tsx
@@ -7,9 +7,22 @@ import { useState } from "react";
 import Menubar from "@/app/shared/menu-bar/menu-bar";
 import { AnimatePresence } from "motion/react";
 import { motion } from "framer-motion";
+import SearchBar from "@/app/shared/search-bar/search-bar";
+import backgroundImage from "@/app/assets/searchbg.png";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isSearch, setIsSearch] = useState(false);
+
+  const handleSearch = () => {
+    setIsSearch(!isSearch);
+    setIsOpen(false);
+  };
+
+  const handleMenu = () => {
+    setIsOpen(!isOpen);
+    setIsSearch(false);
+  };
 
   return (
     <div className={`${Headers.glassBg} fixed w-full top-0 left-0 z-50`}>
@@ -28,23 +41,19 @@ const Header = () => {
         {/* right side content */}
         <div className="flex items-center gap-4">
           <div>
-            <Search className="text-[#009dff]" />
+            <Search onClick={handleSearch} className="text-[#009dff]" />
           </div>
           <div>
             {isOpen ? (
-              <X onClick={() => setIsOpen(false)} />
+              <X onClick={handleMenu} className="text-[#009dff]" />
             ) : (
-              <AlignRight
-                onClick={() => setIsOpen(true)}
-                className="text-[#009dff]"
-              />
+              <AlignRight onClick={handleMenu} className="text-[#009dff]" />
             )}
           </div>
         </div>
       </div>
 
-      {/* menubar */}
-
+      {/* === fullscreen menubar === */}
       <AnimatePresence>
         {isOpen && (
           <motion.div
@@ -55,6 +64,24 @@ const Header = () => {
             className="w-screen h-[calc(100vh-40px)] md:h-[calc(100vh-80px)] fixed top-[4.3rem] md:top-[4.3rem] left-0 bg-[#cce5f6] overflow-y-auto"
           >
             <Menubar />
+          </motion.div>
+        )}
+      </AnimatePresence>
+
+      {/* === fullscreen search bar === */}
+      <AnimatePresence>
+        {isSearch && (
+          <motion.div
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.5, delay: 0.1 }}
+            className="w-screen h-[calc(100vh-40px)] md:h-[calc(100vh-80px)] fixed top-[4.3rem] md:top-[4.2rem] left-0 bg-[#cce5f6] overflow-y-auto"
+            style={{
+              backgroundImage: `url(${backgroundImage.src})`,
+            }}
+          >
+            <SearchBar />
           </motion.div>
         )}
       </AnimatePresence>
