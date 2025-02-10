c73223df0027dc63d52faf2787e5d72e10b797dd
mohammednazim007 <mohammednazim3629@gmail.com>
2025-02-10 17:17:10 +0600

    vertical-timeline component is done

--- src/app/components/hero-vertical-review/hero-vertical-reviews.tsx
+++ src/app/components/hero-vertical-review/hero-vertical-reviews.tsx
@@ -1,7 +1,62 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { ArrowRight } from "lucide-react";
 import React from "react";
 
+// custom types definition for react-vertical-timeline-component
+declare module "react-vertical-timeline-component" {
+  export class VerticalTimeline extends React.Component<any, any> {}
+  export class VerticalTimelineElement extends React.Component<any, any> {}
+}
+
 const HeroVerticalReviews = () => {
-  return <div></div>;
+  return (
+    <div>
+      <VerticalTimeline>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          date="2011 - present"
+          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          icon={<ArrowRight />}
+        >
+          <h3 className="vertical-timeline-element-title">Creative Director</h3>
+          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+          <p>
+            Creative Direction, User Experience, Visual Design, Project
+            Management, Team Leading
+          </p>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date="2010 - 2011"
+          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          icon={<ArrowRight />}
+        >
+          <h3 className="vertical-timeline-element-title">Art Director</h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            San Francisco, CA
+          </h4>
+          <p>
+            Creative Direction, User Experience, Visual Design, SEO, Online
+            Marketing
+          </p>
+        </VerticalTimelineElement>
+
+        {/* <VerticalTimelineElement
+    iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+    icon={<StarIcon />}
+  /> */}
+      </VerticalTimeline>
+    </div>
+  );
 };
 
 export default HeroVerticalReviews;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Header from "./components/Header/header";
 import HeroCallingBanner from "./components/hero-calling-banner/hero-calling-banner";
 import HeroPrivateBanner from "./components/hero-private-banner/hero-private-banner";
 import HeroSecureBanner from "./components/hero-secure-banner/hero-secure-banner";
+import HeroVerticalReviews from "./components/hero-vertical-review/hero-vertical-reviews";
 import HeroVideoBanner from "./components/hero-video-banner/hero-video-banner";
 import HeroBanner from "./components/HeroBanner/heroBanner";
 
@@ -16,6 +17,7 @@ export default function Home() {
       <ClientChatBanner />
       <HeroPrivateBanner />
       <HeroSecureBanner />
+      <HeroVerticalReviews />
     </div>
   );
 }
