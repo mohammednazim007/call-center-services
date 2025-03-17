f5200aa0f6809060e4e7feb99c9addb83b276e60
mohammednazim007 <mohammednazim3629@gmail.com>
2025-03-17 19:11:13 +0600

    fix(hero-banner): adjust initial animation properties for improved visual effect

--- src/app/components/HeroBanner/heroBanner.tsx
+++ src/app/components/HeroBanner/heroBanner.tsx
@@ -57,9 +57,9 @@ const HeroBanner = () => {
         {/* Right side image content */}
         <AnimatePresence>
           <motion.div
-            initial={{ opacity: 0, y: 10 }}
+            initial={{ opacity: 0, y: -30 }}
             whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.5 }}
+            transition={{ duration: 0.9 }}
             className="w-full md:w-auto mt-[4rem] md:mt-[0rem]"
           >
             <Image
